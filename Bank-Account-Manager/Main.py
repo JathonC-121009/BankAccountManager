f0b738b5e9f9f78062b7ac22c56f70a7c053e7b7
JathonC-121009 <jathonc09@gmail.com>
2026-08-05 21:10:39 -0400

    Delete Bank-Account-Manager/Main.py

--- Bank-Account-Manager/Main.py
+++
@@ -1,7 +0,0 @@
-from Account import Account
-from BusinessAccount import BusinessAccount
-
-myBusinessAccount = BusinessAccount("Jathon", 100.00, "business")
-myBusinessAccount._returnBalance()
-myBusinessAccount._increaseBalance(200.00)
-myBusinessAccount._returnBalance()
